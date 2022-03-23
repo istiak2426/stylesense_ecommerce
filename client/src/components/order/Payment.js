cd535d0da49e0270ec3bcf0a88fe5ec1977c1d6c
istiak2426 <you@example.com>
2022-03-23 15:23:39 +0600

    first commit

---
+++ client/src/components/order/Payment.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+import { initPayment} from '../../api/apiOrder'
+import { userInfo  } from "../../utils/auth"
+import {Link} from "react-router-dom"
+
+const Payment = () => {
+
+	const [sessionSuccess, setSessionSuccess] = useState(false)
+	const [failed, setFailed] = useState(false)
+	const [redirectUrl, setRedirectUrl] = useState('');
+
+	useEffect(()=>{
+
+		initPayment(userInfo().token)
+		.then(response=>{
+			if(response.data.status === 'SUCCESS'){
+				setSessionSuccess(true);
+				setRedirectUrl(response.data.GatewayPageURL);
+				setFailed(false);
+			}
+		})
+		.catch(err =>{
+			setFailed(true);
+			setSessionSuccess(false)
+		})
+	})
+
+  return (
+	<div>
+		{sessionSuccess ? window.location = redirectUrl
+		: "Payment is processing..."}
+		{
+			failed ? (<><p>Failed to start payment 
+				sesion...</p><Link to ="/cart">
+					Go to Cart
+				</Link></>) : ""
+		}
+	</div>
+  )
+}
+
+export default Payment
---
+++ client/src/utils/config.js
@@ -0,0 +1,1 @@
+export const API = "https://stylesense2426.herokuapp.com/api"
